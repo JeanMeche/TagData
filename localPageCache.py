0e68b869c2c96ad113c8f501cc065a876092516e
Kyro38 <kyro38@gmail.com>
2014-02-25 00:03:40 +0100

    localPageCache debug + comments

--- localPageCache.py
+++ localPageCache.py
@@ -1,32 +1,44 @@
+"""
+    Simple Python module to store queried web page localy to retrieve them faster.
+     
+
+    The cached files are stored in a given directory and name with their md5 hash. 
+    Their creation date is stored in the same directory in a JSON file.
+"""
+
 import hashlib,json 
 from os import listdir, makedirs
 from os.path import isfile, join, isdir, getmtime
 from urllib.request import urlopen
-from datetime import datetime
+from datetime import datetime, timedelta
 
-cacheDataFilePath = "cacheData"
+_cacheDir = "pythonWebCache/"
+_cacheDataFilePath = _cacheDir+"cacheData.json"
 
-if isfile(cacheDataFilePath) : 
-	file = open(cacheDataFilePath,"r")
+# Will update the file from the server if the cached file is older than the limit.
+timelimit = timedelta (days = 7)
+
+if isfile(_cacheDataFilePath) : 
+	file = open(_cacheDataFilePath,"r")
 	s = file.read()
-	cacheData = json.loads(s)
+	_cacheData = json.loads(s)
 else : 
-	cacheData = dict()
+	_cacheData = dict()
 
-def urlHash(url) :
-    h = hashlib.md5()
-    h.update(url.encode('utf-8'))
-    return h.hexdigest()
 
+"""
+    This is the only public function. Just call it the retrieve the page either from the web or from the cache
+    The origin is determined by the module itself.
+"""
 def getPage(url):
-	md5hash = urlHash(url)
+	md5hash = _urlHash(url)
 
-	if not isdir("pythonWebCache"):
-		makedirs("pythonWebCache")
+	if not isdir(_cacheDir):
+		makedirs(_cacheDir)
 	
-	filename = "pythonWebCache/"+md5hash 
+	filename = _cacheDir+md5hash 
 
-	if isfile(filename) and updateTime(url) : 
+	if isfile(filename) and not _needsUpdate(url) : 
 		print("Loading page",url, "from cache")
 		file = open(filename,"r")
 		return file.read()
@@ -39,29 +51,49 @@ def getPage(url):
 		file = open(filename, "w")
 		file.write(s)
 		file.close()
-		setUpdateTime(url)
+		_setUpdateTime(url)
 		return s
 	return # never reached
+
+
+"""
+    Return a md5 hash for an url (or any other string)
+"""
+def _urlHash(url) :
+    h = hashlib.md5()
+    h.update(url.encode('utf-8'))
+    return h.hexdigest()
 	
-	
-def setUpdateTime(url): 
-	hash = urlHash(url)
-	cacheData[hash] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+"""
+    sets the new creation time of a cached url and updates the cacheData json 
+"""        
+def _setUpdateTime(url): 
+	hash = _urlHash(url)
+    
+    # storing as a string since datetime is not serializable
+	_cacheData[hash] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")) 
 	
 	# Saving the data 
-	jsonData = json.dumps(cacheData,indent=2,sort_keys=True)
-	text_file = open(cacheDataFilePath, "w")
+	jsonData = json.dumps(_cacheData,indent=2,sort_keys=True)
+	text_file = open(_cacheDataFilePath, "w")
 	text_file.write(jsonData)
 	text_file.close()
 
-def updateTime(url):
-	hash = urlHash(url)
-	if hash in cacheData:
-		return datetime.strptime(cacheData[hash],"%Y-%m-%d %H:%M:%S")
+"""
+    retrieves from the cacheData the creation time of the cached url 
+"""
+def _updateTime(url):
+	hash = _urlHash(url)
+	if hash in _cacheData:
+        # The date is stored as a text, it need to be parsed to be a datetime
+		return datetime.strptime(_cacheData[hash],"%Y-%m-%d %H:%M:%S")
 	else:
 		return None
 
-def needsUpdate(url):
+"""
+    Computes if the cache needs to be updated for the given url
+"""
+def _needsUpdate(url):
 	now = datetime.now()
-	then = updateTime(url)
-	return (now - then) > timedelta (days = 14)
+	then = _updateTime(url)
+	return (now - then) > timelimit
