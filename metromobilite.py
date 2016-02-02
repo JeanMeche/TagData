c2dfc8a7e2c2384ce9a160aa6f0252c669fa409c
Matthieu Riegler <matthieu.riegler@viseo.com>
2016-02-02 18:33:02 +0100

    MapPoints & script update

--- metromobilite.py
+++ metromobilite.py
@@ -8,9 +8,14 @@ import functools
 from collections import namedtuple, defaultdict
 from itertools import groupby
 from math import radians, sin, cos, asin, sqrt
+from json import encoder
+import difflib
+
+encoder.FLOAT_REPR = lambda o: format(o, '.5f')
 
 lines = None
 stations = None
+groups = list()
 
 def main(argv):
     print(sys.version)
@@ -34,7 +39,7 @@ def main(argv):
         if len(connections):
             for aConnection in connections:
                 if haversine(float(aConnection['lon']), float(aConnection['lat']), float(aTramStation['lon']), float(aTramStation['lat'])) < 20:
-                    aTramStation['type'] = 'both'
+                    aTramStation['type'] = 'Mixte'
                     aTramStation['source'].extend(aConnection['source'])
                     aTramStation['zones'].extend(aConnection['zones'])
                     mergedBusStations.remove(aConnection)
@@ -43,37 +48,78 @@ def main(argv):
     allStations.extend(mergedTramStations)
     allStations.extend(mergedBusStations)
 
+    toRemove = list()
     for aStation in allStations:
-        aStation.pop("road", None)
+        # allStationstion.pop("road", None)
         aStation.pop("city", None)
+        aStation["id"] = "".join([x.split("_")[1] for x in aStation["source"]])
 
-    with open('MergedStations.json', 'w') as fp:
-        json.dump(allStations, fp, sort_keys=True)
+        stationsToMerge = [x for x in allStations if aStation is not x and distanceBetweenStations(x, aStation) < 20]
+        lat=aStation["lat"]
+        lon=aStation["lon"]
+
+        for i, x in enumerate(stationsToMerge):
+            lat = lat + x["lat"]
+            lon = lon + x["lon"]
+            aStation["zones"].extend(x["zones"])
+            aStation["zones"] = list(set(aStation["zones"]))
+            aStation["type"] = mergeTypes(aStation["type"], x["type"])
+            aStation["source"].extend(x["source"])
+            allStations.remove(x)
+
+        aStation["lat"] = lat/(len(stationsToMerge)+1)
+        aStation["lon"] = lon/(len(stationsToMerge)+1)
+
+    # grouping
+    groups = dict()
+    for aStation in allStations: 
+        nearStations = [x for x in allStations if distanceBetweenStations(x, aStation) < 200]
+        matchingStations = difflib.get_close_matches(aStation["name"], [x["name"] for x in nearStations], n=len(nearStations), cutoff=0.8)
+        for aNearStation in nearStations:
+            if aNearStation["name"] in matchingStations:
+                if "group" not in aNearStation:
+                    aNearStation["group"] = aStation["name"]
+        # print(aStation["name"])
+        # print("\033[92m"+str(matchingStations) +"\033[0m")
+        # print("\033[91m"+str([x for x in nearStations if x not in matchingStations])+"\033[0m")
+        # print("-------------")
+
+    with open('MapPoints.json', 'w') as fp:
+        json.dump(allStations, fp, sort_keys=True, indent=2)
+
+
+def mergeTypes(type1, type2):
+    if type1 == type2:
+        return type1
+    elif type1 == "Tram" or type2 == "Tram":
+        return "Mixte"
+    else:
+        return "Bus"
 
 def stationWithTheNearestStation(stationList):
     size = len(stationList)
-    #table = [[0 for x in range(size)] for x in range(size)]
     minDistance = sys.float_info.max
-    minIndex = None
+    minIndex = 0
     for i in range(size):
         for j in range(size):
-            distance = haversine(stationList[i].location.longitude, stationList[i].location.latitude, stationList[j].location.longitude, stationList[j].location.latitude)
-            if distance < minDistance:
-                minIndex = i
+            if i != j:
+                distance = haversine(stationList[i].location.longitude, stationList[i].location.latitude, stationList[j].location.longitude, stationList[j].location.latitude)
+                if distance < minDistance:
+                    minIndex = i
+                    minDistance = distance
     return stationList[minIndex]
 
-
 def clusterStations(stationList):
-    maxRange = 40
+    maxRange = 60 
     clusters = list()
 
     averageLat = sum(station.location.latitude for station in stationList)/len(stationList)
     averageLon = sum(station.location.longitude for station in stationList)/len(stationList)
 
-    if 'Grand\'Place' in stationList[0].name or 'Neyrpic - Belledonne' in stationList[0].name or 'Flandrin - Valmy' in stationList[0].name:
+    if 'GRAND\'PLACE' in stationList[0].name or 'NEYRPIC' in stationList[0].name or 'FLANDRIN - VALMY' in stationList[0].name:
         dd = defaultdict(list)
         for d in stationList:
-            dd[(d['road'])].append(d)
+            dd[(d.road)].append(d)
         result = list()
         result.append(stationList)
         return dd.values()
@@ -85,27 +131,45 @@ def clusterStations(stationList):
                 break
         else:
             if len(set([station.road for station in stationList])) is 1:  # if every stations are on the same road
-                print(stationList[0].name + ' will be merged')
                 result = list()
                 result.append(stationList)
                 return result
 
+    bb = False
+    # if "LA TRONCHE, CIMETIERE" in stationList[0].name and list(stationList[0].lines)[0].mode == "BUS":
+    #     bb = True
+
     while stationList:
         cluster = list()
         station = stationWithTheNearestStation(stationList)
-        if 'La Revir' in stationList[0].name:
-            print(station)
         stationList.remove(station)
         cluster.append(station)
+        if bb:
+            print("-- ", station)
+
+        stationList.sort(key = lambda p: haversine(station.location.longitude, station.location.latitude, p.location.longitude, p.location.latitude))
+        if bb: 
+            pprint(stationList)
 
         for anotherStation in stationList:
-            if (haversine(station.location.longitude, station.location.latitude, anotherStation.location.longitude, anotherStation.location.latitude) < maxRange and
-                    anotherStation.road == cluster[0].road):
+            averageLoc = averageLocation(cluster)
+            if bb:
+                print(haversine(averageLoc["lon"], averageLoc["lat"], anotherStation.location.longitude, anotherStation.location.latitude), anotherStation)
+                print((anotherStation.road == cluster[0].road or anotherStation.road == "" or cluster[0].road == ""), cluster[0].road, anotherStation.road)
+            if (haversine(averageLoc["lon"], averageLoc["lat"], anotherStation.location.longitude, anotherStation.location.latitude) < maxRange and
+                    (anotherStation.road == cluster[0].road or anotherStation.road == "" or cluster[0].road == "")) or haversine(averageLoc["lon"], averageLoc["lat"], anotherStation.location.longitude, anotherStation.location.latitude) < 15:
                 cluster.append(anotherStation)
-                stationList.remove(anotherStation)
-        clusters.append(cluster)
 
+        stationList = [x for x in stationList if x not in cluster]
+        clusters.append(cluster)
+        if bb:
+            print("Cluster :", cluster ,"\n\n")
     return clusters
+
+def averageLocation(stationList):
+    averageLat = sum(station.location.latitude for station in stationList)/len(stationList)
+    averageLon = sum(station.location.longitude for station in stationList)/len(stationList)
+    return {"lon":averageLon, "lat":averageLat}
 
 
 def mergeTramStations(tramStations):
@@ -130,12 +194,12 @@ def mergeTramStations(tramStations):
         averageLon = sum(station.location.longitude for station in aCluster)/len(aCluster)
 
         newStation = dict()
-        newStation['lat'] = "{:.7f}".format(averageLat)
-        newStation['lon'] = "{:.7f}".format(averageLon)
+        newStation['lat'] = averageLat
+        newStation['lon'] = averageLon
         newStation['city'] = aCluster[0].city
         newStation['name'] = aCluster[0].name
         newStation['source'] = [station.id for station in aCluster]
-        newStation['type'] = 'tram'
+        newStation['type'] = 'Tram'
         newStation["zones"] = list(set([station.zone for station in aCluster]))
         newStation['road'] = aCluster[0].road
 
@@ -152,32 +216,45 @@ def mergeBusStations(busStations):
         for a in group:
             cluster.append(a)
         busStationsClusters.append(cluster)
-
+        
     geoClusters = list()
     for aCluster in busStationsClusters:
         clusters = clusterStations(aCluster)
         geoClusters.extend(clusters)
 
+    # pprint(geoClusters)
+
     mergedBusStations = list()
     for aCluster in geoClusters:
-
         averageLat = sum(station.location.latitude for station in aCluster)/len(aCluster)
         averageLon = sum(station.location.longitude for station in aCluster)/len(aCluster)
 
         newStation = dict()
-        newStation['lat'] = "{:.7f}".format(averageLat)
-        newStation['lon'] = "{:.7f}".format(averageLon)
+        newStation['lat'] = averageLat
+        newStation['lon'] = averageLon
         newStation['city'] = aCluster[0].city
         newStation['name'] = aCluster[0].name
         newStation['source'] = [station.id for station in aCluster]
         newStation["zones"] = list(set([station.zone for station in aCluster]))
-        newStation['type'] = 'bus'
-        newStation['road'] = aCluster[0].road
+        
+        types = set([x.type for x in aCluster])
+        if len(types) == 1:
+            newStation["type"] = types.pop()
+        else:
+            if "Tram" in types:
+                newStation["type"] = "Mixte"
+            else:
+                newStation["type"] = "Bus"
 
+        newStation['road'] = aCluster[0].road
         mergedBusStations.append(newStation)
 
     # pprint(mergedBusStations)
     return mergedBusStations
+
+
+def distanceBetweenStations(station1, station2):
+    return haversine(station1["lon"], station1["lat"], station2["lon"], station2["lat"])
 
 
 def getPointArrets():
@@ -198,7 +275,7 @@ def getPointArrets():
 
 def getLines():
     global lines
-    url = "http://data.metromobilite.fr/otp/routers/default/index/routes"
+    url = "http://data.metromobilite.fr/api/routers/default/index/routes" #"http://data.metromobilite.fr/otp/routers/default/index/routes"
     s = localPageCache.getPage(url)
     linesJson = json.loads(s)
     lines = {x["id"].replace(":", "_"): Line(x) for x in linesJson}
@@ -206,7 +283,7 @@ def getLines():
 
 class Station:
     def __init__(self, jsonObject, lines):
-        if "city" not in jsonObject:
+        if "geometry" in jsonObject:
             coords = jsonObject["geometry"]["coordinates"]
             properties = jsonObject["properties"]
             Location = namedtuple('Location', 'latitude longitude')
@@ -218,7 +295,7 @@ class Station:
             self.name = properties["LIBELLE"]
             self.locateStation()
         else:
-            self.lines = {lines[x] for x in jsonObject["lines"]}
+            self.lines = {lines[x] for x in jsonObject["lines"] if x in lines}
             self.zone = jsonObject["zone"]
             self.pmr = jsonObject["pmr"]
             self.name = jsonObject["name"]
@@ -228,25 +305,39 @@ class Station:
             coords = jsonObject["location"]
             Location = namedtuple('Location', 'latitude longitude')
             self.location = Location(latitude=coords["latitude"], longitude=coords["longitude"])
+            types = set([x.type for x in self.lines])
+            if len(types) == 0:
+                types = set(["Bus"])
+            
+            if len(types) == 1:
+                self.type = types.pop()
+            else:
+                if "Tram" in types:
+                    self.type = "Mixte"
+                else:
+                    self.type = "Bus"
 
     def __str__(self):
         return self.name + str([(x.name, x.mode) for x in self.lines])
 
     def __repr__(self):
-        return self.name + str([(x.name, x.mode) for x in self.lines])
+        return self.name + " " + self.zone + " " + str([(x.name, x.mode) for x in self.lines])
+
+    def distanceFromStation(station):
+        return haversine(station.location.lon, station.location.lat, self.location.lon, self.location.lat)
 
     def hasTrams(self):
+        if len(self.lines) == 0:
+            return False 
         for aLine in self.lines:
             if not aLine.isTram():
                 return False
         return True
 
     def locateStation(self):
-        sys.stdout.write('.')
-        sys.stdout.flush()
         nominatimUrl = "http://open.mapquestapi.com/nominatim/v1/reverse.php?key=NpfVO4ocnBw3PfHSrVCqpGeLzyy4F515&osm_type=N&accept-language=fr&format=json&&lat=" + str(self.location.latitude) + "&lon=" + str(self.location.longitude)
         # nominatimUrl = "http://nominatim.openstreetmap.org/reverse?format=json&osm_type=N&accept-language=fr&lat=" + str(self.location.latitude) + "&lon=" + str(self.location.longitude)
-        nominatimData = localPageCache.getPage(nominatimUrl, true)
+        nominatimData = localPageCache.getPage(nominatimUrl, True)
         print(".", end="")
         nominatimJson = None
         try:
@@ -293,6 +384,7 @@ class StationJSONEncoder(json.JSONEncoder):
         aDict["zone"] = obj.zone
         return aDict
 
+
 class Line:
     def __init__(self, jsonObject):
         self.id = jsonObject["id"].replace(":", "_")
@@ -300,6 +392,7 @@ class Line:
         self.longName = jsonObject["longName"]
         self.mode = jsonObject["mode"]
         self.name = jsonObject["shortName"]
+        self.type = jsonObject["type"]
 
     def __repr__(self):
         return str(self.name + " - " + self.mode)
@@ -315,6 +408,12 @@ class Line:
 
     def isC38(self):
         return "C38" in self.id
+
+    def isScolaire(self):
+        return 
+
+    def isNavette(self):
+        return self.name.isdigit() and int(self.name) > 80 and int(self.name) < 90
 
     def isCableCar(self):
         return self.Mode == "CABLE_CAR"
